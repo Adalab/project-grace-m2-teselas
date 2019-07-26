77b30d8e448a0b92a0a79f64a921da05268ac3b8
rocioferro <fer.rodriguezrocio@gmail.com>
2019-07-26 13:00:32 +0200

    Prepare main.js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+//inputs que reaccionen
+const inputName = document.querySelector('.field__fill-name');
+const inputJob = document.querySelector('.field__fill-job');
+const name = document.querySelector('.name__card');
+const job = document.querySelector('.job__card');
+
+function fillName (event) {
+  name.innerHTML = event.currentTarget.value;
+}
+
+//function fillJob
+inputName.addEventListener ('keyup', fillName);
